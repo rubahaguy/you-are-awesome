1f49e326f3fa08bbbc598b13a66339617fa08446
rubahaguy <36646724+rubahaguy@users.noreply.github.com>
2018-09-12 17:52:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,73 @@
 // DO WHATEVER YOU WANT HERE
 
 const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createNotEnumerableProperty = (propName) => {
+    Object.defineProperty(Object.prototype, propName, {
+        get: () => {
+            return propName;
+        },
+        set: (prop) => {
+            propName = prop;
+        },
+        enumerable: false});
+    return propName;
+};
+const createProtoMagicObject = () => {
+    const notObj = function() {
+
+    }
+    notObj.__proto__ = notObj.prototype;
+    return notObj;
+};
+Function.prototype.valueOf = function() {return counter};
+let counter = 0;
+const incrementor = () => {
+    counter++;
+    return incrementor;
+
+};
+let asyncCounter = 0;
+const asyncIncrementor = () => {
+    return new Promise((resolve) => {
+        asyncCounter++;
+        return resolve(asyncCounter);
+    })
+};
+const createIncrementer = () => {
+    return {
+        value: 0,
+        next: function() {
+            this.value++;
+            console.log(this.value);
+            return this;
+        },
+        [Symbol.iterator]: function() {
+            return {
+                next: () => {
+                    return this.next()
+                },
+            };
+        }
+    }
+
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const returnBackInSecond = (param) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(param);
+        }, 1100)
+    });
+};
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const createSerializedObject = () => {
+    return null;
+};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => {
+    return arr.sort((a, b) => (a.__proto__ - b.__proto__));
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
